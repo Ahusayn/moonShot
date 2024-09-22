88e0363064cc254e24b663b0098646340b2278b3
Abdul <ahusaynn@gmail.com>
2024-09-22 12:24:35 +0300

    Initial Commit

---
+++ moonShot/moonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  moonShotApp.swift
+//  moonShot
+//
+//  Created by mac on 22/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct moonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
